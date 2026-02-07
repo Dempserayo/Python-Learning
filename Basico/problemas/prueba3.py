4b3b6a72a48379003db4ec10773199c4e7b71651
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-02-07 00:41:00 -0500

    Add new functions to prueba3.py for even/odd checks, age filtering, and a basic calculator. Implement user input for operations and enhance output clarity with additional print statements.

--- Basico/problemas/prueba3.py
+++ Basico/problemas/prueba3.py
@@ -46,8 +46,61 @@ c = int(input("Ingrese un numero: "))
 
 lista_total = [a,b,c]
 def sumatotal(lista_total):
-    x = 0
-    for i in lista_total:
-        x = i + x
+    print(lista_total[0] + lista_total[1] + lista_total[2])
 
 sumatotal(lista_total)
+
+
+
+
+lista = [1,2,3,4,5]
+def sumar_pares (lista):
+    valor = 0
+    for i in lista:
+        valor = i
+        if valor % 2 == 0:
+            print(i, "es numero par")
+        else:
+            print(i, "es numero impar")
+
+sumar_pares(lista)
+
+
+
+edades = [12,14,22,23,8]
+print(edades)
+
+def edade_mayores (edades):
+    nueva_edades = []
+    for i in edades:
+        if i >=18:
+            nueva_edades.append(i)
+            print(nueva_edades)
+
+edade_mayores(edades)
+ 
+
+
+
+
+a = int(input("Ingrese valor: "))
+b = int(input("Ingrese valor: "))
+c = int(input("1.Suma 2.Resta 3.Multiplicacion 4.Division: "))
+
+operacion2 = [a,b,c]
+
+def calculadora (operacion2):
+    if c == 1:
+        print(a + b)
+    elif c == 2:
+        print(a - b)
+    elif c == 3:
+        print(a * b)
+    elif c == 4:
+        print(a / b)
+    else:
+        print("Valores Incorrectos")
+
+calculadora(operacion2)
+
+
