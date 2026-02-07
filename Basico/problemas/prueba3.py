8fcc363b31728e32213d7e4f02d56c441c2cb00c
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-02-06 19:02:42 -0500

    Add new functions to prueba3.py for summation, parity check, and comparison. Implement user input for multiple numbers and calculate their total. Enhance output clarity with additional print statements.

--- Basico/problemas/prueba3.py
+++ Basico/problemas/prueba3.py
@@ -10,3 +10,44 @@ def fibonacci(limit):
 
 for num in fibonacci(55):
     print(num)
+
+
+def sumar(a,b):
+    return a + b
+
+resultado = sumar(2,2)
+print(resultado)
+
+
+
+def numeros (a):
+    if a / 2 == 0:
+        print("El numero:",a,"es impar")
+    else:
+        print("El numero:",a,"es par")
+        
+
+numeros(9)        
+
+def mayorque(a,b):
+    if a > b:
+        print(a,"es mayor que",b)
+    else:
+        print(b,"es mayor que",a)
+
+mayorque(2,4)
+
+
+
+
+a = int(input("Ingrese un numero: "))
+b = int(input("Ingrese un numero: "))
+c = int(input("Ingrese un numero: "))
+
+lista_total = [a,b,c]
+def sumatotal(lista_total):
+    x = 0
+    for i in lista_total:
+        x = i + x
+
+sumatotal(lista_total)
